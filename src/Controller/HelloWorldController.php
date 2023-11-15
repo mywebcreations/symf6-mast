f5f83addf94a01ba38aed7d9846b791b5fc92225
Adedoyin Adedipe <mywebcreativities@gmail.com>
2023-11-15 16:56:41 +0100

    SSM-7 added route using attribute

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloWorldController extends AbstractController
 {
+    #[Route('/', name: 'app_helloWorld')]
     public function helloWorld(): Response {
         return new Response("Hello World!");
     }
