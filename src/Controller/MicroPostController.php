e2ef06c7c193d20aba4f668b3dd672507c619bc5
Adedoyin Adedipe <mywebcreativities@gmail.com>
2024-01-09 08:14:53 +0100

    SSM-18 persist to entity in controller

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\MicroPostRepository;
+use App\Entity\MicroPost;
+use DateTimeInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +13,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(
+        EntityManagerInterface $em, 
+        MicroPostRepository $microPostRepo
+    ): Response
     {
+        // dd($microPostRepo->findAll());
+        
+        // $microPost = new MicroPost();
+        // $microPost->setTitle('Welcome to Netherlands');
+        // $microPost->setText('The Netherlands is a fantastic country to visit. We are also praying for it.');
+        // $microPost->setCreated(new \DateTime());
+        
+        $microPost = $microPostRepo->find(1);
+        $microPost->setTitle("Hearty welcome to Belgium");        
+        
+        $em->persist($microPost);
+        $em->flush();
+
+        $this->addFlash('success', 'Update of MicroPost successfful');
+
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
