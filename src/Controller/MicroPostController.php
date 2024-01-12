8f2426e3833a4d6216a9e18e7388a0be125cf24d
Adedoyin Adedipe <mywebcreativities@gmail.com>
2024-01-12 13:01:49 +0100

    SSM-26 modified controller and added content to template file

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -12,6 +12,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MicroPostController extends AbstractController
 {
+    #[Route('/micro-post', name: 'app_micro_post')]
+    public function index(
+        EntityManagerInterface $em, 
+        MicroPostRepository $microPostRepo
+    ): Response
+    {
+        $posts = $microPostRepo->findAll();
+        
+        // $microPost = new MicroPost();
+        // $microPost->setTitle('Welcome to Netherlands');
+        // $microPost->setText('The Netherlands is a fantastic country to visit. We are also praying for it.');
+        // $microPost->setCreated(new \DateTime());
+        
+        // $microPost = $microPostRepo->find(1);
+        // $microPost->setTitle("Hearty welcome to Belgium");        
+        
+        // $em->persist($microPost);
+        // $em->flush();
+
+        // $this->addFlash('success', 'Update of MicroPost successfful');
+
+        return $this->render(
+            'micro_post/index.html.twig', [
+               'posts' => $posts
+            ]
+        );
+    }
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        return $this->render(
+            'micro_post/show-one.html.twig', [
+               'post' => $post
+            ]
+        );
+    }
+
     #[Route('/micro-post/create', name: 'app_micro_post_create')]
     public function createPost(): Response
     {
@@ -21,8 +59,8 @@ class MicroPostController extends AbstractController
             ->add('text')
             ->getForm();
         
-        return $this->render('micro_post/post.html.twig', [
-            'form' => $microPost,
+        return $this->render('micro_post/create-post.html.twig', [
+            'form' => $form,
         ]);
     }
 
